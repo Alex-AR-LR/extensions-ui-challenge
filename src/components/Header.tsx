257fd883f8009a970cbefe4f27e8ab28be89e094
Ale <exequiel2695@outlook.com>
2026-01-14 01:49:58 -0300

    Intentando implementar componentes reutilizables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { Logo } from "../icons";
+
 export const Header = () => {
   return (
     <header>
-      <h1>Hola mundo</h1>
+      <div className="border">
+        <Logo />
+      </div>
     </header>
   );
 };
